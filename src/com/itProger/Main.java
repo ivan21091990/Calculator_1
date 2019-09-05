35a6c1e005b59c5bc96c4f014743a3efde49cbb1
IPAndreev <aip1990andreev@yandex.ru>
2019-09-05 22:07:21 +0300

    Change

--- src/com/itProger/Main.java
+++ src/com/itProger/Main.java
@@ -1,10 +1,12 @@
 package com.itProger;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите первое число:");
         double x = scanner.nextDouble();
@@ -12,32 +14,33 @@ public class Main {
         double y = scanner.nextDouble();
         System.out.println("Доступные операции: ");
         System.out.print("+\n-\n*\n/\n");
-        char z;
 
-        do {
-            System.out.print("Выберите: ");
-            z = scanner.next().charAt(0);
-        } while (z == ('+' | '-' | '*' | '/'));
-             switch (z) {
-                 case '+':
-                     System.out.printf(x + " + " + y + " = %.4f", (x + y));
-                     break;
-                 case '-':
-                     System.out.printf(x + " - " + y + " = %.4f", (x - y));
-                     break;
-                 case '*':
-                     System.out.printf(x + " * " + y + " = %.4f", (x * y));
-                     break;
-                 case '/':
-                     if (y == 0) {
-                         System.out.println("На 0 делить нельзя");
-                     } else {
-                         System.out.printf(x + " / " + y + " = %.4f", (x / y));
-                         break;
-                     }
+        System.out.print("Выберите операцию: ");
+        char z = scanner.next().charAt(0);
 
+        switch (z) {
+            case '+':
+                System.out.printf(x + " + " + y + " = %.4f", (x + y));
+                break;
+            case '-':
+                System.out.printf(x + " - " + y + " = %.4f", (x - y));
+                break;
+            case '*':
+                System.out.printf(x + " * " + y + " = %.4f", (x * y));
+                break;
+            case '/':
+                if (y == 0) {
+                    System.out.println("На 0 делить нельзя");
+                } else {
+                System.out.printf(x + " / " + y + " = %.4f", (x / y));
+                break;
+                }
+             default:
+                 System.out.println("Неверная операция");
              }
 
         }
+
     }
+
 
